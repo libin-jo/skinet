17be6e0de3e4f85a5bbafcafeff2b3bd36aa5696
Libin Joseph <libin.jo@outlook.com>
2022-07-01 23:43:37 +0530

    first commit

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+    }
+}
